d0f98ed6ce20499a82fdd2d2aabf9650bce34fb2
EstuardoValenzuela <estuardovg@hotmail.com>
2020-04-23 22:06:37 -0600

    Laboratorio #9

--- Laboratorio #9/main.cpp
+++ Laboratorio #9/main.cpp
@@ -1,105 +1,136 @@
 #include <iostream>
 #include <stdio.h>
-#include <string>
+#include <conio.h>
 #include <vector>
-#include <math.h>
 
-using namespace std;
+/*
+Laboratorio #9
+Informatica II
+Fecha de entrega: 23 de Abril, 2020 @ 11:59pm
+*/
 
+class LinkedList {
 
-//----------------------------------------Tarea #1----------------------------------------------- 
-float parseFloat(const string& numreal) {              	    //Enviare parametros por referencia y retornare un float
-	float resultado = strtof(numreal.c_str(), nullptr);		//La funcion strfot me ayuda a convertir un string a un float
-	return resultado;										//Retorno resultado
-}
-
-
-//---------------------------------------Tarea #2------------------------------------------------
-vector<string> coma(string str, char nuevo = ',') {          //Debo definir una funcion auxiliar para separar el string
-
-	int inicio = 0;
-	int iterador = 0;										//Esta variable la utilizare para el bucle While
-	string divisor;
-	vector<string> total;
+    public:
 	
-	while (iterador >= 0) {
-		iterador = str.find(nuevo, inicio);
-		divisor = str.substr(inicio, iterador - inicio);
-		inicio = iterador+1;
-		total.push_back(divisor);
-	}
-	return total;
-}
-
-class Vector3d {
-
-public:
-
-	Vector3d(string coordenadas) {
-		vector<string> punto=coma(coordenadas);
-		corx = parseFloat(punto[0]);
-		cory = parseFloat(punto[1]);
-		corz = parseFloat(punto[2]);
-	}
-
-//----------------------------------------Tarea #3--------------------------------------------------------
-	float distancia(Vector3d aux) {
-		float distanciax = pow((corx - aux.corx), 2);
-		float distanciay = pow((cory - aux.cory), 2);
-		float distanciaz = pow((corz - aux.corz), 2);
-
-		return sqrt(distanciax + distanciay + distanciaz);
-	}
-//---------------------------Este apartado me ayudara a realizar las pruebas------------------------------
-	float getX() {
-		return corx;
-	}
-	float getY() {
-		return cory;
-	}
-	float getZ() {
-		return corz;
-	}
-//-----------------------------------------------------------------------------------------------------
-private:
-
-	float corx;
-	float cory;
-	float corz;
-}; 
-
-int main(){
-//-----------------------------------------------Tarea #4-----------------------------------------------
-	vector<Vector3d> vectorn;
-	string xnum;
-	float total = 0;
-	int iterador = 0;
-
-	cout << "Introduzca los vectores: \n";
-
-	while (xnum != "\n") {
-		getline(cin, xnum);         
-		if (!xnum.empty()) {
-			vectorn.push_back(Vector3d(xnum));
-			iterador++;
-		}
-		else {
-			break;
-		}
-	}
-	if (vectorn.empty()) {
-		total = 0;
-	}
-	else {
-		for (int i=0; i<iterador-1; i++) {
-			total +=vectorn[i].distancia(vectorn[i+1]);
-		}
+	LinkedList(){
+    }
+	
+    int getvalor() {
+		return valor;
 	}
 	
-	cout << "La distancia es: " <<total;
+	LinkedList* getList() {
+		return fin;
+	}
+    
+    int listalargo() {//cuenta la cantidad de elementos en una lista
+        LinkedList* encadenada = (LinkedList*)malloc(sizeof(LinkedList));
+        encadenada->x = x;
+        encadenada->fin = fin;
+	    int i= 1;
+	    if (encadenada->x==0 && encadenada->fin==nullptr) {
+		    return 0;
+	    }
+    	else {
+		    while (encadenada->fin != nullptr) {
+			    i++;
+			encadenada = encadenada->fin;
+		    }
+		    return i;
+	    }
+    }
+    /*
+    Problema #1 (20%): Definir el metodo push que agrega un elemento al final de la lista encadenada.
+    */
+       void push(int valor){
+		   
+        LinkedList *reserva = new LinkedList;
 
-	char corx;
-	std::cin >> corx;
+        reserva->x=valor;
+        reserva->fin=nullptr;
+        
+        LinkedList* encadenada=this;
+        
+        while(encadenada->fin != nullptr){
+              encadenada = encadenada->fin;
+        }
+		
+        
+        encadenada->fin = reserva;
+    }
+    
+    /*
+    Problema #2 (20%): Definir el metodo "toVector" el cual debe retornar un vector
+    con los mismos elementos que esta lista encadenada.
+    */
+    vector<int> toVector(){
+		
+        vector<int> vector;
+        int valor = x;
+		
+        LinkedList* arr = fin;
 
-	return 0;
-}
+		while (resto != nullptr) {
+				vector.push_back(valor);
+				valor = arr->x;
+				arr = arr->fin;
+		}
+		
+		vector.push_back(valor);
+
+		return vector;
+    }
+
+    /*
+    Problema #3 (20%): Definir el "operador []" para la lista encadenada. Este operador debe retornar
+    una referencia al indice dado como parametro. Puede asumir que los indices siempre son
+    validos.
+    https://en.cppreference.com/w/cpp/language/operators#Array_subscript_operator
+    */
+
+    int& operator[](const int indice){
+        LinkedList* encadenada=this;
+        for(int i=0;i<indice;i++){
+            encadenada = encadenada->fin;
+        }
+        return encadenada-x;
+    }
+
+    /*
+    Problema #4: Definir el destructor para esta lista encadenada. Asegurese que
+    todos los valores de la lista sean correctamente eliminados siguiendo
+    los punteros que sean necesarios.
+    https://en.cppreference.com/w/cpp/language/destructor
+    */
+    ~LinkedList(){
+		x = 0;
+		delete fin;
+
+    /*
+    Problema #5: Definir el "operador =" tal que copie todos los valores de la lista
+    actual a la nueva lista. Asegurese que:
+     * Se crea una nueva copia de cada uno de los elementos de la lista con este operador.
+     * Se libera la memoria ocupada por los valores anteriores
+    https://en.cppreference.com/w/cpp/language/copy_assignment
+    */
+    LinkedList& operator=(const LinkedList& otra)
+        x=0;
+        delete fin;
+        fin = nullptr;
+        int ex = aux.listalargo();
+        x = aux.x;//El value, será el valor de la otra lista
+        
+        for(int i = 1; i<ex; i++){
+            push(aux[i]);
+        }
+        return *this;
+    }
+
+   
+	 private:
+    int x;
+    LinkedList* fin;
+
+
+};
